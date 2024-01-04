7ae41b9d15dd6b94591cf29f13d28689eec8add5
DiSerafim <diegoserafim1@gmail.com>
2024-01-04 18:21:30 -0300

    criado arquivo config para guardar a Porta de acesso da URL Localhost

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const app = require("./app");
 
+const dotenv = require("dotenv");
+
+// Config
+dotenv.config({path: "backend/config/config.env"});
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor trabalahando na porta http://localhost:${process.env.PORT}`);
