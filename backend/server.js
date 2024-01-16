f04a2f1d9779b578d3704340b44487e275bf6927
DiSerafim <diegoserafim1@gmail.com>
2024-01-15 21:37:40 -0300

    importando DB no server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
 // Config
 dotenv.config({path: "backend/config/config.env"});
+
+// Conecta ao banco de dados
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor trabalahando na porta http://localhost:${process.env.PORT}`);
