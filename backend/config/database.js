d9b2e743eff95f9bd5cc26c1c1398aef46d5faa8
DiSerafim <diegoserafim1@gmail.com>
2024-01-16 21:05:37 -0300

    ajustes e codigo utrapassado removido

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    }).then((data) => {
+    mongoose.connect(process.env.DB_URI).then((data) => {
         console.log(`Mongodb Conectado com o Servidor: ${data.connection.host}`);
     }).catch((err) => {
-        console.log(err);
+        console.log(err, "EROAQUI");
     })
 }
 
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
 // Config
-dotenv.config({path: "backend/config/config.env"});
+dotenv.config({path:"backend/config/config.env"});
 
 // Conecta ao banco de dados
 connectDatabase();
