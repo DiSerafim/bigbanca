e9b19ca5803798d5e329fc5fc5cdda62c7dc9576
DiSerafim <diegoserafim1@gmail.com>
2024-01-23 20:22:08 -0300

    Atualiza produto pelo seu id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productModel");
 
-// Cria o Produto no DB
+// Cria um produto
 exports.createProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
     res.status(201).json({
@@ -9,7 +9,34 @@ exports.createProduct = async (req, res, next) => {
     })
 }
 
-exports.getAllProducts = (rec, res) => {
-
-    res.status(200).json({ message: "A rota está funcionando bem" })
+// Mostra todos os produtos
+exports.getAllProducts = async (rec, res) => {
+    const products = await Product.find();
+    res.status(200).json({
+        message: "A rota está funcionando bem",
+        success: true,
+        products
+    })
 };
+
+// Atualiza um produto
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Produto não encontrado"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
