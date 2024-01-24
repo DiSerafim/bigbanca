0892133ac78eeb2908b28fd2b2ab4169eed4747a
DiSerafim <diegoserafim1@gmail.com>
2024-01-24 20:46:17 -0300

    criado middleware/error

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,17 @@ require('dotenv').config()
 const express = require ("express");
 const app = express();
 
+const errorMiddleware = require("./middleware/error");
+
 app.use(express.json());
 
 // Importa as rotas
 const product = require("./routes/productRoute");
 
+// API
 app.use("/api/v1", product);
+
+// Middleware para tratar erros de conexão Http
+app.use(errorMiddleware);
 
 module.exports = app
