701ff782654811b9598611d38325596cef882a89
DiSerafim <diegoserafim1@gmail.com>
2024-01-25 21:45:22 -0300

    Criada funções para tratar erros exceções não capturadas & Promessa Não Tratada

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URI).then((data) => {
-        console.log(`Mongodb Conectado com o Servidor: ${data.connection.host}`);
-    }).catch((err) => {
-        console.log(err, "EROAQUI");
-    })
-}
+    mongoose
+        .connect(process.env.DB_URI)
+        .then((data) => {
+            console.log(`Mongodb Conectado com o Servidor: ${data.connection.host}`);
+        })
+        .catch((err) => {
+            console.log(err, "EROAQUI");
+        });
+};
 
 module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
-const app = require("./app");
 require('dotenv').config({ path:"/backend/config/config.env" })
-
-// Config
-// dotenv.config({ path:"/backend/config/config.env" });
-
+const app = require("./app");
 const connectDatabase = require("./config/database");
+
+// Trata exceções não capturadas
+process.on("uncaughtException", (err) => {
+    console.log(`Erro: ${err.message}`);
+    console.log(`Desligando o servidor devido a exceções não capturadas`);
+    process.exit(1);
+});
 
 // Conecta ao banco de dados
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Servidor trabalhando na porta http://127.0.0.1:${process.env.PORT}`);
+});
+
+// Rejeição de Promessa Não Tratada
+process.on("unhandledRejection", (err) => {
+    console.log(`Erro: ${err.message}`);
+    console.log(`Desligando o servidor devido a uma Rejeição de Promessa Não Tratada`);
+
+    server.close(() => {
+        process.exit(1);
+    });
 });
