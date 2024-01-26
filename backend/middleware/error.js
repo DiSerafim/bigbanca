0e9ac42d27d9e583ab913474e1f7e53404549ba4
DiSerafim <diegoserafim1@gmail.com>
2024-01-26 20:29:24 -0300

    criado condição para Erro de ID incorreto do MongoDB

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,12 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Erro interno do servidor.";
 
+    // Erro de ID incorreto do MongoDB
+    if (err.name === "CastError") {
+        const message = `Erro de ID incorreto ou Inválido: ${err.path}`;
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
