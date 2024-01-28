7ff247d1c6309708be46580e696bc8c250dd8bd3
DiSerafim <diegoserafim1@gmail.com>
2024-01-27 21:41:25 -0300

    criada donfição para filtrar produtos, 'preço, category'

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,7 +16,9 @@ exports.createProduct = catchAsyncErrors(
 
 // Mostra todos os produtos
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-    const apiFeature = new ApiFeatures(Product.find(), req.query).search();
+    const apiFeature = new ApiFeatures(Product.find(), req.query)
+        .search()
+        .filter();
         const products = await apiFeature.query;
         res.status(200).json({
             success: true,
--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -17,6 +17,22 @@ class ApiFeatures {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    // Filtra
+    filter() {
+        const queryCopy = { ... this.queryStr };
+        
+        // Remove alguns campos da categoria
+        const removeFields = ["keyword", "page", "limit"];
+        removeFields.forEach((key) => delete queryCopy[key]);
+
+        // Filtrar por preço e classificação
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
 }
 
 module.exports = ApiFeatures;
