d05d61faef78dbef9ec9598e5974896e80ee8d20
DiSerafim <diegoserafim1@gmail.com>
2024-02-03 12:25:41 -0300

    criada função para desconectar usuário

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,18 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     if (!isPasswordMatched) {
         return next(new ErrorHandler("Email ou senha Inválido", 401));
     }
-
     // JWT token
     sendToken(user, 200, res);
+});
+
+// Desconecta usuário
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+    res.status(200).json({
+        success: true,
+        message: "Você foi desconectado",
+    });
 });
