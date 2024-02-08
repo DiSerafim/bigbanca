bf70c9f4de375f432725ec0cde969c3f8af7deb5
DiSerafim <diegoserafim1@gmail.com>
2024-02-07 22:53:18 -0300

    adicionar, atualizar e excluir produto, somente com user 'admin'

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -8,6 +8,6 @@ const {
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/logout").get(logoutUser)
+router.route("/logout").get(logoutUser);
 
 module.exports = router;
