9847446f8e4bd3fd57099280557f5c2d9211394d
DiSerafim <diegoserafim1@gmail.com>
2024-02-13 18:51:15 -0300

    criada condições de erro de JWT

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,6 +10,24 @@ module.exports = (err, req, res, next) => {
         err = new ErrorHandler(message, 400);
     }
 
+    //  Erro de chave duplicada do Mongoose
+    if (err.code === 11000) {
+        const message = `Desculpe, este ${Object.keys(err.keyValue)} já está em uso.`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Erro de JWT errado
+    if (err.name === "JsonWebTokenError") {
+        const message = `Json Web Token é inválido, Tente novamente`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Erro de JWT expirado
+    if (err.name === "TokenExpiredError") {
+        const message = `Json Web Token expirou, Tente novamente`;
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
