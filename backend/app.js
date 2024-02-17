2e72044f1f8a0e538c8b3914779f850ad044c2bb
DiSerafim <diegoserafim1@gmail.com>
2024-02-17 13:13:11 -0300

    criada função para Criar novo Pedido 'Order'

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,12 @@ app.use(cookieParser());
 // Importa as rotas
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 // API
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 // Middleware para tratar erros de conexão Http
 app.use(errorMiddleware);
