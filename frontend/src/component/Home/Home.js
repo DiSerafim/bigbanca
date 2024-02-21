295c668b5954620662a3b22cc8acb8e9cc5942a7
DiSerafim <diegoserafim1@gmail.com>
2024-02-21 17:39:42 -0300

    Criada Página de produtos e o card do produto

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,6 +1,14 @@
 import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
+import Product from "./Product.js";
+
+const product = {
+    name: "Blue Tshirt",
+    images: [{ url: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRW1-Fu4baINkJ6XPPCD2okRQmI0313bGJGE0WNYvqzDecWwdyJJ_s8qtPDFyCNnlbTd6JBJYcn0-wZ4lOWuygSrdwLEnvuAeshmZUOcpng5aLHKqy1TLGlkyEXvG7SqT3mAGKpi4M&usqp=CAc" }],
+    price: "R$ 3.000,00",
+    _id: "diego",
+}
 
 const Home = () => {
     return (
@@ -11,12 +19,24 @@ const Home = () => {
 
                 <a href="#container">
                     <button>
-                    Rolagem <CgMouse />
+                    Ver <CgMouse />
                     </button>
                 </a>
             </div>
 
             <h2 className="homeHeading">Produtos em Destaque</h2>
+
+            <div className="container" id="container">
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+                <Product product={product} />
+            </div>
         </Fragment>
     );
 };
---
+++ frontend/src/component/Home/Product.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
+
+const options = {
+    edit: false,
+    color: "rgba(20, 20, 20, 0.1)",
+    activeColor: "crimson",
+    size: window.innerWidth < 600 ? 10 : 15,
+    value: 1.5,
+    isHalf: true,
+};
+
+const Product = ({ product }) => {
+    return(
+        <Link className="productCard" to={product._id}>
+            <img src={product.images[0].url} alt={product.name} />
+            <p>{product.name}</p>
+            <span>{product.price}</span>
+            <div>
+                <ReactStars {...options} className="react-stars" />
+                <span>256</span>
+            </div>
+        </Link>
+
+
+    );
+}
+
+export default Product;
