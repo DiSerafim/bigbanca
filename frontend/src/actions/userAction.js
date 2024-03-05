00bdbabf43f6d1f911a96dbb443de420fb0629e2
DiSerafim <diegoserafim1@gmail.com>
2024-03-05 19:15:58 -0300

    criada ação para Atualizar Perfil

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -12,6 +12,10 @@ import {
     LOAD_USER_SUCCESS,
     LOGOUT_FAIL,
     LOGOUT_SUCCESS,
+    UPDATE_PROFILE_FAIL,
+    UPDATE_PROFILE_REQUEST,
+    UPDATE_PROFILE_RESET,
+    UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants";
 
 // Login Usuário
@@ -62,6 +66,18 @@ export const logout = () => async (dispatch) => {
         dispatch({ type: LOGOUT_SUCCESS });
     } catch (error) {
         dispatch({ type: LOGOUT_FAIL, payload: error.response.data.message });
+    }
+};
+
+// Atualizar Perfil
+export const updateProfile = (userData) => async (dispatch) => {
+    try {
+        dispatch({ type: UPDATE_PROFILE_REQUEST });
+        const config = { headers: { "Content-Type": "multipart/form-data" } };
+        const { data } = await axios.put(`/api/v1/me/update`, userData, config);
+        dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.user });
+    } catch (error) {
+        dispatch({ type: UPDATE_PROFILE_FAIL, payload: error.response.data.message, });
     }
 };
 
