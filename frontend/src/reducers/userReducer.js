b91ce5019c3e5caee2d7724eea65ee93b7540246
DiSerafim <diegoserafim1@gmail.com>
2024-03-07 18:52:36 -0300

    criada redux para atualizar senha de usuário

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -15,6 +15,10 @@ import {
     UPDATE_PROFILE_REQUEST,
     UPDATE_PROFILE_RESET,
     UPDATE_PROFILE_SUCCESS,
+    UPDATE_PASSWORD_FAIL,
+    UPDATE_PASSWORD_REQUEST,
+    UPDATE_PASSWORD_RESET,
+    UPDATE_PASSWORD_SUCCESS,
 } from "../constants/userConstants";
 
 // Redux -- Login Usuário / Cadastro Usuário / Carrega Usuário
@@ -75,27 +79,31 @@ export const userReducer = (state = { user: {} }, action) => {
     }
 };
 
-// Redux -- Atualiza Perfil Usuário
+// Redux -- Atualiza Perfil/Senha do Usuário
 export const profileReducer = (state = {}, action) => {
     switch (action.type) {
         case UPDATE_PROFILE_REQUEST:
+        case UPDATE_PASSWORD_REQUEST:
             return {
                 ...state,
                 loading: true,
             };
         case UPDATE_PROFILE_SUCCESS:
+        case UPDATE_PASSWORD_SUCCESS:
             return {
                 ...state,
                 loading: false,
                 isUpdated: action.payload,
             };
         case UPDATE_PROFILE_FAIL:
+        case UPDATE_PASSWORD_FAIL:
             return {
                 ...state,
                 loading: false,
                 error: action.payload,
             };
         case UPDATE_PROFILE_RESET:
+        case UPDATE_PASSWORD_RESET:
             return {
                 
             };
