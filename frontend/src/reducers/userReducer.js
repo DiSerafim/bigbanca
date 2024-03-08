7f57560708956fa07c89c26c855c4fcfb424928f
DiSerafim <diegoserafim1@gmail.com>
2024-03-08 20:18:27 -0300

    criado redux para senha esquecida

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -19,6 +19,9 @@ import {
     UPDATE_PASSWORD_REQUEST,
     UPDATE_PASSWORD_RESET,
     UPDATE_PASSWORD_SUCCESS,
+    FORGOT_PASSWORD_FAIL,
+    FORGOT_PASSWORD_REQUEST,
+    FORGOT_PASSWORD_SUCCESS
 } from "../constants/userConstants";
 
 // Redux -- Login Usuário / Cadastro Usuário / Carrega Usuário
@@ -105,7 +108,40 @@ export const profileReducer = (state = {}, action) => {
         case UPDATE_PROFILE_RESET:
         case UPDATE_PASSWORD_RESET:
             return {
-                
+                ...state,
+                isUpdated: false,
+            };
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null,
+            };
+    
+        default:
+            return state;
+    }
+};
+
+// Redux -- Senha Esquecida
+export const forgotPasswordReducer = (state = {}, action) => {
+    switch (action.type) {
+        case FORGOT_PASSWORD_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                error: null,
+            };
+        case FORGOT_PASSWORD_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                message: action.payload,
+            };
+        case FORGOT_PASSWORD_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
             };
         case CLEAR_ERRORS:
             return {
