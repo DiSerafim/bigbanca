e2b675ec3bcbc0a30cd20d4fc20dbad6d5ec6d43
DiSerafim <diegoserafim1@gmail.com>
2024-03-09 11:30:54 -0300

    removido 'console.log'

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -39,7 +39,6 @@ class ApiFeatures {
         const currentPage = Number(this.queryStr.page) || 1;
         const skip = resultPerPage * (currentPage - 1);
         this.query = this.query.limit(resultPerPage).skip(skip);
-        console.log(this);
         return this;
     }
 }
