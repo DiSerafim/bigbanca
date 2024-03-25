fa13349f374df5c9cffdbb23c0685967118f85e7
DiSerafim <diegoserafim1@gmail.com>
2024-03-24 21:37:59 -0300

    configurado 'stripe' para pagamento por catão

--- backend/app.js
+++ backend/app.js
@@ -16,11 +16,13 @@ app.use(fileUpload());
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
 
 // API
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
+app.use("/api/v1", payment);
 
 // Middleware para tratar erros de conexão Http
 app.use(errorMiddleware);
