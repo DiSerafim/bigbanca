cac164072bc45f25235a4e97e3a91607575371ed
DiSerafim <diegoserafim1@gmail.com>
2024-08-27 20:26:45 -0300

    Ajustado o limite do tamanho da imagem para envio

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,8 @@ const fileUpload = require("express-fileupload");
 
 const errorMiddleware = require("./middleware/error");
 
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
