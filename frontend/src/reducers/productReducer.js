52c0ba9319059754fd0f9dd5bce5ab0de1f7d622
DiSerafim <diegoserafim1@gmail.com>
2024-08-28 20:07:57 -0300

    criada reducer para editar produtos

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -20,7 +20,11 @@ import {
     DELETE_PRODUCT_REQUEST,
     DELETE_PRODUCT_SUCCESS,
     DELETE_PRODUCT_FAIL,
-    DELETE_PRODUCT_RESET
+    DELETE_PRODUCT_RESET,
+    UPDATE_PRODUCT_REQUEST,
+    UPDATE_PRODUCT_SUCCESS,
+    UPDATE_PRODUCT_FAIL,
+    UPDATE_PRODUCT_RESET
 } from "../constants/productConstants";
 
 // Redux Para Exibir Produtos Usuário Comum e Admin
@@ -133,6 +137,42 @@ export const deleteProductsReducer = (state = {}, action) => {
             return state;
     };
 };
+
+// Redux Para Atualizar Produtos
+export const updateProductReducer = (state = {}, action) => {
+    switch (action.type) {
+        case UPDATE_PRODUCT_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case UPDATE_PRODUCT_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                isUpdated: action.payload,
+            };
+        case UPDATE_PRODUCT_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+        case UPDATE_PRODUCT_RESET:
+            return {
+                ...state,
+                isUpdated: false,
+            };
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null,
+            };
+        default:
+            return state;
+    };
+};
+
 
 // Redux Para Exibir Detalhes do Produto
 export const productDetailsReducer = (state = { product: {} }, action) => {
